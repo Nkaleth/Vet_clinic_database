30f48db570b394752a2416e14e41a4298ef83879
Nkaleth <nseguralu@gmail.com>
2023-03-29 08:40:19 -0500

    ADD queries and transactions with screenshots

--- schema.sql
+++ schema.sql
@@ -4,8 +4,9 @@ CREATE TABLE animals (
     id INT GENERATED ALWAYS AS IDENTITY,
 	name VARCHAR(15) NOT NULL,
 	date_of_birth DATE NOT NULL,
-    escape_attempts INT NOT NULL, 
+  escape_attempts INT NOT NULL, 
 	neutered BOOLEAN NOT NULL,
 	weight_kg DECIMAL NOT NULL,
+	species VARCHAR(15),
     PRIMARY KEY (id)
 );
