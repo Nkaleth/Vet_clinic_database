28bb5ef46809a5a015d26d78b9e25f32096c43a5
Nkaleth <nseguralu@gmail.com>
2023-03-29 10:45:41 -0500

    FIX: Add non-optional changes according last review.

--- schema.sql
+++ schema.sql
@@ -1,12 +1,14 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT GENERATED ALWAYS AS IDENTITY,
+  id INT GENERATED ALWAYS AS IDENTITY,
 	name VARCHAR(15) NOT NULL,
 	date_of_birth DATE NOT NULL,
   escape_attempts INT NOT NULL, 
 	neutered BOOLEAN NOT NULL,
 	weight_kg DECIMAL NOT NULL,
-	species VARCHAR(15),
     PRIMARY KEY (id)
 );
+
+ALTER TABLE animals
+ADD COLUMN species varchar(255);
